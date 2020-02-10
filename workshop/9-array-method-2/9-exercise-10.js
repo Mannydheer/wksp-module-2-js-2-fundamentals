5698a6e4df102c7675cdda139f973cbbc53bd632
GD-DevFlow <gindulink@gmail.com>
2020-02-09 22:36:37 -0500

    javascript 2

--- workshop/9-array-method-2/9-exercise-10.js
+++ workshop/9-array-method-2/9-exercise-10.js
@@ -10,6 +10,64 @@
 
 // Write function here
 
+//must declare outside as GLOBAL 
+let averageCal = 0;
+
+
+function calculateAverage(grades) {
+    //declare vaiables. 
+    let storeGrade = 0;
+    //will keep track fo how many index in the arrays. 
+    let countEach = 0;
+        grades.forEach(function(gradeItem)
+        {
+            //store each grade into the storegrade var as iterate through foreach. 
+           storeGrade += gradeItem
+           countEach++;
+        });
+    
+    //calcualte the avg.
+    avgCal = Math.floor(storeGrade/countEach);
+
+    //IF ELSE WILL CHECK AND RETURN CORRECT LETTER GRADE. 
+    if (avgCal < 60) {
+      return "F";
+    }
+    else if (avgCal <70) {
+      return "D";
+    }
+    else if (avgCal < 80) {
+      return "C";
+    }
+    else if (avgCal < 90) {
+      return "B";
+    }
+    else if (avgCal  < 100) {
+      return "A";
+    }
+
+
+
+
+   
+    
+    }
+    
+    
+    //return C
+    console.log(calculateAverage([76, 60, 83, 100, 78]));
+
+
+    // return D
+    console.log(calculateAverage([48, 95, 65, 48, 59, 78, 72, 65]));
+
+
+
+
+
+
+
+
 
  
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
--- workshop/9-array-method-2/9-exercise-9.js
+++ workshop/9-array-method-2/9-exercise-9.js
@@ -2,9 +2,27 @@
 // Write a function that accepts an array of grades (numerical values) and returns the average.
 // The returned average should also be rounded to the nearest unit. (no decimals, please!)
 
+
+
 function calculateAverage(grades) {
+    //declare vaiables. 
+    let storeGrade = 0;
+    //will keep track fo how many index in the arrays. 
+    let countEach = 0;
+   
+        grades.forEach(function(gradeItem)
+        {
+            //store each grade into the storegrade var as iterate through foreach. 
+           storeGrade += gradeItem
+           countEach++;
+           
     
-
-}
-
-console.log(calculateAverage([76, 60, 83, 100, 78]));
+        });
+    
+    //calcualte the avg and round it. 
+    return Math.floor(storeGrade/countEach);
+    
+    }
+    
+    console.log(calculateAverage([76, 60, 83, 100, 78]));
+    
